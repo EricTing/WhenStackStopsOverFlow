39d7350bff07cbb35cb06407114d520d73b76f3b
EricTing <yunding.whu@gmail.com>
2016-06-15 00:54:00 -0500

    fix the badge data for each question
    	modified:   badges_data.py

--- badges_data.py
+++ badges_data.py
@@ -1,6 +1,41 @@
 #!/usr/bin/env python
 
+import luigi
 import pandas as pd
+
+
+class ExtractBadge(luigi.Task):
+    starting_date = luigi.Parameter()
+
+    def output(self):
+        ofn = "earned_badges.{}.json".format(self.starting_date)
+        return luigi.LocalTarget(ofn)
+
+    def run(self):
+        starting_date = self.starting_date
+        qa = pd.read_json("./extracted.{}.json".format(starting_date))
+        questioner = pd.read_json("./questioner.json")
+        questioner.index = range(questioner.shape[0])
+
+        qaq = pd.merge(qa, questioner, left_on='id', right_on='id')
+        badges = pd.read_json("./badges.json")
+
+        def foo(row):
+            owneruserid = row['owneruserid']
+            creationdate = row['creationdate']
+            my_badges = ' '.join(badges[(badges['userid'] == owneruserid) & (
+                badges['date'] < creationdate)]['name'])
+            return my_badges
+
+        earned = qaq.apply(foo, axis=1)
+        print("earned shape: {}".format(earned.shape))
+        print("{} out {} does not have badges".format(earned[
+            earned == ''].shape[0], earned.shape[0]))
+        qaq['badges'] = earned
+        df = qaq[['badges', 'owneruserid', 'id']]
+        df.columns = ['badges', 'userid', 'id']
+        df.to_json(self.output().path)
+
 
 def main(starting_date):
     qa = pd.read_json("./extracted.{}.json".format(starting_date))
@@ -13,11 +48,12 @@ def main(starting_date):
     badges = pd.read_json("./badges.json")
 
     qaq_b = pd.merge(qaq,
-                    badges[['date', 'name', 'userid']],
-                    left_on='owneruserid',
-                    right_on='userid')
+                     badges[['date', 'name', 'userid']],
+                     left_on='owneruserid',
+                     right_on='userid')
 
-    qaq_b = qaq_b[qaq_b['date'] < qaq_b['creationdate'].astype('datetime64[ns]')]
+    qaq_b = qaq_b[qaq_b['date'] < qaq_b['creationdate'].astype(
+        'datetime64[ns]')]
 
     earned_badges = qaq_b.groupby(['userid', 'id']).apply(
         lambda g: ' '.join(g['name']))
@@ -28,11 +64,13 @@ def main(starting_date):
     df['badges'] = earned_badges.values
 
     df.to_json("earned_badges.{}.json".format(starting_date),
-            date_format='iso')
+               date_format='iso')
 
 
 if __name__ == '__main__':
-    starting_date = "2016-02-01"
-    main(starting_date)
-    starting_date = "2015-11-01"
-    main(starting_date)
+    luigi.build(
+        [
+            ExtractBadge(starting_date="2016-02-01"),
+            ExtractBadge(starting_date="2015-11-01"),
+        ],
+        local_scheduler=True)
